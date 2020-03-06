0d29d0960067091f6dead8ed6d73414e8f5bdb56
mcsarathkumar <sarathkumar0510@gmail.com>
2020-03-06 12:19:18 +0530

    2 files modified

--- src/app/pets-description/pets-description.component.ts
+++ src/app/pets-description/pets-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pets-description',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetsDescriptionComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('modal', {static: false}) myModal: ElementRef;
+  constructor(public renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.openModal();
+  }
+
+  openModal() {
+    this.renderer.addClass(this.myModal.nativeElement, 'show');
+    this.renderer.setStyle(this.myModal.nativeElement, 'display', 'block');
+    const div = this.renderer.createElement('div');
+    this.renderer.addClass(div, 'modal-backdrop');
+    this.renderer.addClass(div, 'fade');
+    this.renderer.addClass(div, 'show');
+    this.renderer.setAttribute(div, 'id', 'backdropMyModal');
+    document.getElementsByTagName('body')[0].appendChild(div);
+  }
+
+  closeModal() {
+    this.renderer.removeClass(this.myModal.nativeElement, 'show');
+    this.renderer.setStyle(this.myModal.nativeElement, 'display', 'none');
+    document.getElementById('backdropMyModal').remove();
   }
 
 }
